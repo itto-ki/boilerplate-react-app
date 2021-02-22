1a47c11d539b200fc925e7b2008e75adff4ec4fd
Ittoh Kimura <kimura.itto.kd3@gmail.com>
2021-02-22 21:39:12 +0900

    Feat: initialize webpack

---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.tsx",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "index.js",
+  },
+  devtool: "source-map",
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "src/assets/index.html"),
+    }),
+    new MiniCssExtractPlugin(),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        include: [path.resolve(__dirname, "src")],
+        exclude: [/node_modules/],
+        use: ["babel-loader"],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          "postcss-loader",
+          "sass-loader",
+          "typed-css-modules-loader",
+        ],
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        type: "asset/inline",
+      },
+    ],
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, "dist"),
+    host: "localhost",
+    hot: true,
+    open: true,
+  },
+};
